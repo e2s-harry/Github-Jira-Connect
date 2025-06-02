c3125d2a80da16b7e62a258a046442f8132a6863
harry <harry@edoubles.co.kr>
2025-06-02 16:58:23 +0900

    feat : text 수정

--- app/src/main/java/kr/co/edoubles/harry/jiraconnecttest/MainActivity.kt
+++ app/src/main/java/kr/co/edoubles/harry/jiraconnecttest/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name Branch 10",
+        text = "Hello $name Branch 11",
         modifier = modifier
     )
 }
